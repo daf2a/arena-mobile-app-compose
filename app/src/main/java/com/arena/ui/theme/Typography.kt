700e4a5d7cfa03001fbf27e1571db3b947a0c1df
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2024-07-14 17:19:57 +0700

    style: change font to rethink sans, poppins jelek klo di andro :)

--- app/src/main/java/com/arena/ui/theme/Typography.kt
+++ app/src/main/java/com/arena/ui/theme/Typography.kt
@@ -9,90 +9,90 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.arena.R
 
-val Poppins = FontFamily(
-    Font(R.font.poppins_thin, FontWeight.Thin),
-    Font(R.font.poppins_thinitalic, FontWeight.Thin, style = FontStyle.Italic),
-    Font(R.font.poppins_extralight, FontWeight.ExtraLight),
-    Font(R.font.poppins_extralightitalic, FontWeight.ExtraLight, style = FontStyle.Italic),
-    Font(R.font.poppins_light, FontWeight.Light),
-    Font(R.font.poppins_lightitalic, FontWeight.Light, style = FontStyle.Italic),
-    Font(R.font.poppins_regular, FontWeight.Normal),
-    Font(R.font.poppins_italic, FontWeight.Normal, style = FontStyle.Italic),
-    Font(R.font.poppins_medium, FontWeight.Medium),
-    Font(R.font.poppins_mediumitalic, FontWeight.Medium, style = FontStyle.Italic),
-    Font(R.font.poppins_semibold, FontWeight.SemiBold),
-    Font(R.font.poppins_semibolditalic, FontWeight.SemiBold, style = FontStyle.Italic),
-    Font(R.font.poppins_bold, FontWeight.Bold),
-    Font(R.font.poppins_bolditalic, FontWeight.Bold, style = FontStyle.Italic),
-    Font(R.font.poppins_extrabold, FontWeight.ExtraBold),
-    Font(R.font.poppins_extrabolditalic, FontWeight.ExtraBold, style = FontStyle.Italic),
-    Font(R.font.poppins_black, FontWeight.Black),
-    Font(R.font.poppins_blackitalic, FontWeight.Black, style = FontStyle.Italic)
+val Rethink = FontFamily(
+    Font(R.font.rethinksans_regular, FontWeight.Thin),
+    Font(R.font.rethinksans_italic, FontWeight.Thin, style = FontStyle.Italic),
+    Font(R.font.rethinksans_regular, FontWeight.ExtraLight),
+    Font(R.font.rethinksans_italic, FontWeight.ExtraLight, style = FontStyle.Italic),
+    Font(R.font.rethinksans_regular, FontWeight.Light),
+    Font(R.font.rethinksans_italic, FontWeight.Light, style = FontStyle.Italic),
+    Font(R.font.rethinksans_regular, FontWeight.Normal),
+    Font(R.font.rethinksans_italic, FontWeight.Normal, style = FontStyle.Italic),
+    Font(R.font.rethinksans_medium, FontWeight.Medium),
+    Font(R.font.rethinksans_mediumitalic, FontWeight.Medium, style = FontStyle.Italic),
+    Font(R.font.rethinksans_semibold, FontWeight.SemiBold),
+    Font(R.font.rethinksans_semibolditalic, FontWeight.SemiBold, style = FontStyle.Italic),
+    Font(R.font.rethinksans_bold, FontWeight.Bold),
+    Font(R.font.rethinksans_bolditalic, FontWeight.Bold, style = FontStyle.Italic),
+    Font(R.font.rethinksans_extrabold, FontWeight.ExtraBold),
+    Font(R.font.rethinksans_extrabolditalic, FontWeight.ExtraBold, style = FontStyle.Italic),
+    Font(R.font.rethinksans_extrabold, FontWeight.Black),
+    Font(R.font.rethinksans_extrabolditalic, FontWeight.Black, style = FontStyle.Italic)
 )
 
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Black,
         fontSize = 30.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.ExtraBold,
         fontSize = 24.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.SemiBold,
         fontSize = 18.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Light,
         fontSize = 14.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = Poppins,
+        fontFamily = Rethink,
         fontWeight = FontWeight.Normal,
         fontSize = 10.sp
     )
